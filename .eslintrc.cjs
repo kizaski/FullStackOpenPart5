2c2496e62c994bb2735ccdd1c06c712afcc0bc23
Kiril <kiril2315@gmail.com>
2024-03-05 19:34:08 +0200

    disable trailing spaces errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -32,7 +32,6 @@ module.exports = {
           "never"
       ],
       "eqeqeq": "error",
-      "no-trailing-spaces": "error",
       "object-curly-spacing": [
           "error", "always"
       ],
